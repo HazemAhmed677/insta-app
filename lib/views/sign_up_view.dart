fbccf45d9552ad374e85b6d176b17550d88c24f4
Hazem <hzm@gmail.com>
2024-07-01 05:38:36 +0300

    add professional modal progress hud when signing up

--- lib/views/sign_up_view.dart
+++ lib/views/sign_up_view.dart
@@ -9,6 +9,7 @@ import 'package:insta_app/widgets/custom_ink_well.dart';
 import 'package:insta_app/widgets/custom_question_text.dart';
 import 'package:insta_app/widgets/custom_stack_widget.dart';
 import 'package:insta_app/widgets/custom_text_form_field.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -28,172 +29,186 @@ class _SignUpState extends State<SignUp> {
   bool flag1 = false, flag2 = false, flag3 = false;
   String? email, password;
   File? selectedImage;
-
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     double hight = MediaQuery.of(context).size.height;
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
-          child: Form(
-            key: formKey,
-            child: ListView(
-              physics: const BouncingScrollPhysics(
-                  decelerationRate: ScrollDecelerationRate.fast),
-              controller: controller,
-              children: [
-                SizedBox(
-                  height: 0.125 * hight,
-                ),
-                const Center(
-                  child: Text(
-                    'Insta',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: 'PlaywriteMX',
+      child: ModalProgressHUD(
+        blur: 1.5,
+        color: kBlack,
+        opacity: 0.3,
+        inAsyncCall: isLoading,
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Form(
+              key: formKey,
+              child: ListView(
+                physics: const BouncingScrollPhysics(
+                    decelerationRate: ScrollDecelerationRate.fast),
+                controller: controller,
+                children: [
+                  SizedBox(
+                    height: 0.125 * hight,
+                  ),
+                  const Center(
+                    child: Text(
+                      'Insta',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontFamily: 'PlaywriteMX',
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: hight * 0.025,
-                ),
-                const CustomStackWidget(),
-                SizedBox(
-                  height: hight * 0.025,
-                ),
-                CustomTextFormField(
-                  autovalidateMode: autoValidMode[0],
-                  flag: flag1,
-                  validator: (input) {
-                    if (input!.isEmpty) {
-                      return 'please enter your name';
-                    } else if (input.length < 4) {
-                      return 'enter 4 char at least';
-                    } else {
-                      return null;
-                    }
-                  },
-                  label: 'username',
-                  hint: 'Enter username',
-                  autoFocus: true,
-                ),
-                SizedBox(
-                  height: hight * 0.022,
-                ),
-                CustomTextFormField(
-                  autovalidateMode: autoValidMode[1],
-                  flag: flag2,
-                  validator: (input) {
-                    if (input!.isEmpty) {
-                      return 'please enter your email';
-                    } else if (!input.contains('@')) {
-                      return 'enter a valid email';
-                    } else {
-                      email = input;
-                      return null;
-                    }
-                  },
-                  onTap: () async {
-                    await kAnimateTo(controller);
-                  },
-                  label: 'email',
-                  hint: 'Enter email',
-                ),
-                SizedBox(
-                  height: hight * 0.022,
-                ),
-                CustomTextFormField(
-                  autovalidateMode: autoValidMode[2],
-                  flag: flag3,
-                  onTap: () async {
-                    await kAnimateTo(controller);
-                  },
-                  validator: (input) {
-                    if (input!.isEmpty) {
-                      return 'please enter password';
-                    } else if (input.length < 8) {
-                      return 'enter 8 characters at least';
-                    } else {
-                      password = input;
-                      return null;
-                    }
-                  },
-                  label: 'password',
-                  hint: 'Enter password',
-                  obsecure: obsecure,
-                  passwordIcon: IconButton(
-                    onPressed: () {
-                      obsecure = !obsecure;
-                      setState(() {});
-                    },
-                    icon: (obsecure)
-                        ? const Icon(Icons.visibility_off)
-                        : const Icon(Icons.visibility),
+                  SizedBox(
+                    height: hight * 0.025,
                   ),
-                ),
-                SizedBox(
-                  height: hight * 0.020,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: CustomInkWell(
-                      color: Colors.blue.withOpacity(0.88),
-                      onTap: () async {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState!.save();
-                          // firebase code
-                          try {
-                            await createUser();
-                            if (mounted) {
-                              setState(() {
-                                Navigator.pushNamed(
-                                    context, HomeView.homeViewId);
-                              });
-                            }
-                          } on FirebaseAuthException catch (e) {
-                            if (e.code == 'weak-password') {
-                              if (mounted) {
-                                setState(() {
-                                  getShowSnackBar(context, 'weak password');
-                                });
-                              }
-                            } else if (e.code == 'email-already-in-use') {
-                              if (mounted) {
-                                setState(() {
-                                  getShowSnackBar(
-                                      context, 'email already exists');
-                                });
-                              }
-                            }
-                          } catch (e) {
-                            print(e.toString());
-                          }
-
-                          for (int i = 0; i < 3; i++) {
-                            autoValidMode[i] = AutovalidateMode.disabled;
-                          }
-                        }
-                        for (int i = 0; i < 3; i++) {
-                          autoValidMode[i] = AutovalidateMode.always;
-                          setState(() {});
-                        }
+                  const CustomStackWidget(),
+                  SizedBox(
+                    height: hight * 0.025,
+                  ),
+                  CustomTextFormField(
+                    autovalidateMode: autoValidMode[0],
+                    flag: flag1,
+                    validator: (input) {
+                      if (input!.isEmpty) {
+                        return 'please enter your name';
+                      } else if (input.length < 4) {
+                        return 'enter 4 char at least';
+                      } else {
+                        return null;
+                      }
+                    },
+                    label: 'username',
+                    hint: 'Enter username',
+                    autoFocus: true,
+                  ),
+                  SizedBox(
+                    height: hight * 0.020,
+                  ),
+                  CustomTextFormField(
+                    autovalidateMode: autoValidMode[1],
+                    flag: flag2,
+                    validator: (input) {
+                      if (input!.isEmpty) {
+                        return 'please enter your email';
+                      } else if (!input.contains('@')) {
+                        return 'enter a valid email';
+                      } else {
+                        email = input;
+                        return null;
+                      }
+                    },
+                    onTap: () async {
+                      await kAnimateTo(controller);
+                    },
+                    label: 'email',
+                    hint: 'Enter email',
+                  ),
+                  SizedBox(
+                    height: hight * 0.020,
+                  ),
+                  CustomTextFormField(
+                    autovalidateMode: autoValidMode[2],
+                    flag: flag3,
+                    onTap: () async {
+                      await kAnimateTo(controller);
+                    },
+                    validator: (input) {
+                      if (input!.isEmpty) {
+                        return 'please enter password';
+                      } else if (input.length < 8) {
+                        return 'enter 8 characters at least';
+                      } else {
+                        password = input;
+                        return null;
+                      }
+                    },
+                    label: 'password',
+                    hint: 'Enter password',
+                    obsecure: obsecure,
+                    passwordIcon: IconButton(
+                      onPressed: () {
+                        obsecure = !obsecure;
+                        setState(() {});
                       },
-                      text: 'Sign up'),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: CustomQuestionText(
-                    question: 'do you have an account?',
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
+                      icon: (obsecure)
+                          ? const Icon(Icons.visibility_off)
+                          : const Icon(Icons.visibility),
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: hight * 0.020,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: CustomInkWell(
+                        color: Colors.blue.withOpacity(0.88),
+                        onTap: () async {
+                          if (formKey.currentState!.validate()) {
+                            formKey.currentState!.save();
+                            setState(() {
+                              isLoading = true;
+                            });
+                            // firebase code
+                            try {
+                              await createUser();
+                              setState(() {
+                                isLoading = false;
+                              });
+                              if (mounted) {
+                                setState(() {
+                                  Navigator.pushNamed(
+                                      context, HomeView.homeViewId);
+                                });
+                              }
+                            } on FirebaseAuthException catch (e) {
+                              if (e.code == 'weak-password') {
+                                if (mounted) {
+                                  setState(() {
+                                    isLoading = false;
+                                    getShowSnackBar(context, 'weak password');
+                                  });
+                                }
+                              } else if (e.code == 'email-already-in-use') {
+                                if (mounted) {
+                                  setState(() {
+                                    isLoading = false;
+                                    getShowSnackBar(
+                                        context, 'email already exists');
+                                  });
+                                }
+                              }
+                            } catch (e) {
+                              print(e);
+                            }
+
+                            for (int i = 0; i < 3; i++) {
+                              autoValidMode[i] = AutovalidateMode.disabled;
+                            }
+                          }
+                          for (int i = 0; i < 3; i++) {
+                            autoValidMode[i] = AutovalidateMode.always;
+                            setState(() {});
+                          }
+                        },
+                        text: 'Sign up'),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: CustomQuestionText(
+                      question: 'do you have an account?',
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
