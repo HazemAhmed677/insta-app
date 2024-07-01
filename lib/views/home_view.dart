e7c07ed9e83896316ca5bd5fac2b25a693b7e8fe
Hazem <hzm@gmail.com>
2024-07-01 21:46:40 +0300

    solve log in problem eventially (assign in onChange not validator)

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,11 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:insta_app/constants.dart';
 import 'package:insta_app/helper/modal_progress_hud_helper.dart';
 import 'package:insta_app/helper/show_snack_bar_function.dart';
 import 'package:insta_app/views/sign_in_view.dart';
 import 'package:insta_app/widgets/trigger_switch_cubit.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
